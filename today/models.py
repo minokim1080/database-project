c0821e89c03f959c09c41c6f56bb321405ed2a7d
김민호 <minokim1080@gmail.com>
2021-12-08 13:54:56 +0900

    모델 수정

--- today/models.py
+++ today/models.py
@@ -3,8 +3,8 @@ from today import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
-    gender = db.Column(db.String(1), nullable=False)
+    password = db.Column(db.String(2048), nullable=False)
+    gender = db.Column(db.String(30), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     height = db.Column(db.Float, nullable=False)
     weight = db.Column(db.Float, nullable=False)
