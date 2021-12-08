4ca5879431eebc48569a0dacf1c899c7ff780a65
김민호 <minokim1080@gmail.com>
2021-12-08 13:31:21 +0900

    데이터베이스 수정

--- config/production.py
+++ config/production.py
@@ -2,7 +2,7 @@ from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
         user='dbmasteruser',
-        pw='Q4%*4p5KNxZj9S&O?LQk((VgH5`IkjU9'
+        pw='Q4%*4p5KNxZj9S&O?LQk((VgH5`IkjU9',
         url='ls-73647d1b23a063a11b30fbbb4a0fb48995082594.ce6ming3ugdm.ap-northeast-2.rds.amazonaws.com',
         db='flask_today')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
