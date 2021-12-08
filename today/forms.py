232d3ef7afe579b45ae78eabd882d725a0b4c4c2
김민호 <minokim1080@gmail.com>
2021-12-08 14:03:16 +0900

    데이터베이스 수정

--- today/forms.py
+++ today/forms.py
@@ -3,16 +3,16 @@ from wtforms import StringField, TextAreaField, PasswordField, FloatField, Boole
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class UserCreateForm(FlaskForm):
-    user_id = StringField('아이디', validators=[DataRequired(), Length(min=4, max=20)])
+    user_id = StringField('아이디', validators=[DataRequired()])
     password1 = PasswordField('비밀번호', validators=[DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다')])
     password2 = PasswordField('비밀번호확인', validators=[DataRequired()])
-    gender = StringField('성별', validators=[DataRequired(), Length(min=1, max=3)])
+    gender = StringField('성별', validators=[DataRequired()])
     age = FloatField('나이', validators=[DataRequired()])
     height = FloatField('키(cm)', validators=[DataRequired()])
     weight = FloatField('체중(kg)', validators=[DataRequired()])
 
 class UserLoginForm(FlaskForm):
-    user_id = StringField('ID', validators=[DataRequired('아이디는 필수 입력 항목입니다.'), Length(min=4, max=20)])
+    user_id = StringField('ID', validators=[DataRequired('아이디는 필수 입력 항목입니다.')])
     password = PasswordField('PW', validators=[DataRequired('비밀번호는 필수 입력 항목입니다.')])
 
 
--- today/models.py
+++ today/models.py
@@ -2,9 +2,9 @@ from today import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(30), unique=True, nullable=False)
+    user_id = db.Column(db.String(2048), unique=True, nullable=False)
     password = db.Column(db.String(2048), nullable=False)
-    gender = db.Column(db.String(30), nullable=False)
+    gender = db.Column(db.String(2048), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     height = db.Column(db.Float, nullable=False)
     weight = db.Column(db.Float, nullable=False)
@@ -12,8 +12,8 @@ class User(db.Model):
 
 class Cloth(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    classification = db.Column(db.String(30), nullable=False)
-    cloth_type = db.Column(db.String(30), nullable=False)
-    fit = db.Column(db.String(20), nullable=False)
-    color = db.Column(db.String(20), nullable=False)
+    classification = db.Column(db.String(2048), nullable=False)
+    cloth_type = db.Column(db.String(2048), nullable=False)
+    fit = db.Column(db.String(2048), nullable=False)
+    color = db.Column(db.String(2048), nullable=False)
 
